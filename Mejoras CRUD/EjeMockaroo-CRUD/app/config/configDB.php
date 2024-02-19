b556bd87d124001332d2d3f84359610db755f087
Antoniomanu <104353099+Antoniomanu3@users.noreply.github.com>
2024-02-19 23:41:38 +0100

    Delete Mejoras CRUD/EjeMockaroo-CRUD directory

--- Mejoras CRUD/EjeMockaroo-CRUD/app/config/configDB.php
+++
@@ -1,6 +0,0 @@
-<?php
-const DB_SERVER = '127.0.0.1';
-const DB_USER = 'root';
-const DB_PASSWD = 'root';
-const DATABASE = 'TestMockaroo';
-
--- Mejoras CRUD/EjeMockaroo-CRUD/web/js/funciones.js
+++
@@ -1,9 +0,0 @@
-/**
- * Funciones auxiliares de javascripts 
- */
-function confirmarBorrar(nombre,id){
-  if (confirm("¿Quieres eliminar el cliente:  "+nombre+"?"))
-  {
-   document.location.href="?orden=Borrar&id="+id;
-  }
-}
